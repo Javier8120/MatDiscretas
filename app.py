e76793f45fbb080c228f65c1fc2b0b391bb0142d
Javier <javier.moreno@unicolombo.edu.co>
2021-11-06 23:28:33 -0500

    Algoritmo (Numeros decimales infinitos a fracciones) funcionando en consola

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template as render
+from flask import Flask, render_template as render, flash as alerta
 app = Flask(__name__)
-app.secret_key = 'Equipo8'
+app.secret_key = 'sfnsfisni3n4i3nsowdo2023iofmcim4i4903fm394mfoe'
 
    
 #Rutas generales :v
