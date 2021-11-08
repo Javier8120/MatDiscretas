c9c4907232839efcc74e77703eb6f2a6f6978f3f
Javier <javier.moreno@unicolombo.edu.co>
2021-11-08 00:00:37 -0500

    Problema 1 funcionando correctamente... subiendo a heroku

--- app.py
+++ app.py
@@ -1,10 +1,71 @@
-from flask import Flask, render_template as render, flash as alerta
+from flask import Flask, render_template as render, flash, request, redirect, url_for
+from math import gcd 
 app = Flask(__name__)
 app.secret_key = 'sfnsfisni3n4i3nsowdo2023iofmcim4i4903fm394mfoe'
 
    
 #Rutas generales :v
 
+
 @app.route('/', methods=["GET", "POST"])
 def Login():
-    return render("index.html")
+    flash("Bienvenido", category='info')
+    return render("index.html", methods=["GET", "POST"])
+
+
+@app.route('/Problema1', methods=["GET", "POST"])
+def ruta1():
+    return render("DecimalesInfinitos.html")
+
+
+
+@app.route("/Problema1/procesar", methods=["GET","POST"])
+def procesar():
+    PenteraString = request.form.get("Pentera", int())
+    PeriodoString = request.form.get("Periodo", int())
+    Pentera = int(PenteraString)
+    Periodo = int(PeriodoString)
+
+    lista=[9, 99, 999, 9999, 99999, 999999]
+    error=False
+    if Periodo < 0:
+        error = True
+        flash("Ehhh, la parte fraccionaria no puede ser 0 ", category='info')
+    elif Periodo == 0:
+        error = True
+        flash(":V Si el periodo es 0 entonces la fracciones es la Parte entera sobre 1.(ParteEntera/1)", category='error')
+    elif Periodo >=1000000:
+        flash("Estamos trabajando con numeros entre 1 y 999999")
+    elif Periodo < 10 and Periodo > 0:
+        numerador=lista[0]
+    elif Periodo < 100 and Periodo >= 10:
+        numerador=lista[1]
+    elif Periodo < 1000 and Periodo >= 100:
+        numerador=lista[2]
+    elif Periodo < 10000 and Periodo >= 1000:
+        numerador=lista[3]
+    elif Periodo < 100000 and Periodo >= 10000:
+        numerador=lista[4]
+    elif Periodo < 1000000 and Periodo >= 100000:
+        numerador=lista[5]
+    if Pentera == 0 and error==False:
+        denominador1= Periodo
+        denominador_fraccion1 = (denominador1-Pentera)
+        mcd1 = gcd(denominador1,numerador)
+        denominador_simplificado1 = int(denominador1/mcd1)
+        numerador_simplificado1 = int(numerador/mcd1)
+        fraccion_simplificada1= (f"{denominador_simplificado1}/{numerador_simplificado1}")
+        flash(f"La fraccion resultante de : {Pentera},{Periodo}... es:  {denominador_fraccion1}/{numerador} y la Fraccion simplificada es: {fraccion_simplificada1}", category='success')
+    
+    elif Pentera != 0 and error==False:
+        denominador = int(f"{Pentera}{Periodo}")
+        denominador_fraccion = (denominador-Pentera)
+        mcd = gcd(denominador,numerador)
+        denominador_simplificado = int(denominador/mcd)
+        numerador_simplificado =   int(numerador/mcd)
+        fraccion_simplificada= (f"{denominador_simplificado}/{numerador_simplificado}")
+        flash(f"La fraccion resultante de : {Pentera},{Periodo}...  es: {denominador_fraccion}/{numerador} y la Fraccion simplificada es: {fraccion_simplificada} ", category='success')
+
+
+    return redirect("/Problema1")
+    
--- readme.md
+++ readme.md
@@ -6,12 +6,13 @@ app.secret_key = 'Equipo8'
 
    
 #Crear Ruta
-@app.route('/Login', methods=["GET", "POST"])
-def Login():
-    return render("Login.html")
+@app.route('/Loquesea', methods=["GET", "POST"])
+def Loquesea():
+    return render("loquesea.html")
 
 # .gitignore cumple la funcion de obviar o ignorar las carpetas del proyecto que alli se comenten.
 # index es la vista principal
 # layout es el diseño por defecto, para futuras actualizciones.
+# El archivo Procfile nos genera la conexion con Heroku... para poder desplegar nuestra app en la web 
 
 
